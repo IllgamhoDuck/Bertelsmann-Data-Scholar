b179dbaaafa8be8fa662650f0a5c26846eea2c12
Hyunbyung Park <hyunbyung87@gmail.com>
2020-01-05 17:08:39 -0800

    CONCAT clause practice

--- SQL/SQL_data_cleaning.sql
+++ SQL/SQL_data_cleaning.sql
@@ -106,3 +106,101 @@ FROM sales_reps;
 SELECT LEFT(name, STRPOS(name, ' ') - 1) AS first,
        RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) AS last
 FROM sales_reps;
+
+-------------------
+----- CONCAT ------
+-------------------
+
+/*
+1. Each company in the accounts table wants to create
+an email address for each primary_poc.
+The email address should be the first name of the primary_poc.
+last name primary_poc @ company name .com.
+*/
+
+SELECT name,
+       LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1)
+       || '.'
+       || RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' '))
+       || '@'
+       || name
+       || '.com'
+       AS email
+FROM accounts;
+
+/*
+2. You may have noticed that in the previous solution some of the
+company names include spaces, which will certainly not work in an email address.
+See if you can create an email address that will work by
+removing all of the spaces in the account name,
+but otherwise your solution should be just as in question 1.
+Some helpful documentation is here.
+https://www.postgresql.org/docs/8.1/functions-string.html
+*/
+
+SELECT name,
+       LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1)
+       || '.'
+       || RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' '))
+       || '@'
+       || REPLACE(name, ' ', '')
+       || '.com'
+       AS email
+FROM accounts;
+
+/*
+3. We would also like to create an initial password,
+which they will change after their first log in.
+The first password will be the first letter of the primary_poc''s
+first name (lowercase), then the last letter of their first name (lowercase),
+the first letter of their last name (lowercase),
+the last letter of their last name (lowercase),
+the number of letters in their first name,
+the number of letters in their last name,
+and then the name of the company they are working with,
+all capitalized with no spaces.
+*/
+
+--correct answer
+WITH primary_poc AS (SELECT name,
+                        LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first,
+                        RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) AS last
+                     FROM accounts)
+
+SELECT a.name,
+          LOWER(LEFT(p.first, 1))
+       || LOWER(RIGHT(p.first, 1))
+       || LOWER(LEFT(p.last, 1))
+       || LOWER(RIGHT(p.last, 1))
+       || LENGTH(p.first)
+       || LENGTH(p.last)
+       || UPPER(REPLACE(a.name, ' ', ''))
+       AS password
+FROM accounts a
+JOIN primary_poc p
+ON a.name = p.name;
+
+--another version
+WITH primary_poc AS (SELECT name,
+                        LEFT(primary_poc, STRPOS(primary_poc, ' ') - 1) AS first,
+                        RIGHT(primary_poc, LENGTH(primary_poc) - STRPOS(primary_poc, ' ')) AS last
+                     FROM accounts),
+     company AS (SELECT name,
+                        LEFT(name, STRPOS(name, ' ') - 1) AS first,
+                        RIGHT(name, LENGTH(name) - STRPOS(name, ' ')) AS last
+                 FROM accounts)
+
+SELECT a.name,
+          LOWER(LEFT(p.first, 1))
+       || LOWER(LEFT(p.last, 1))
+       || LOWER(LEFT(c.first, 1))
+       || LOWER(LEFT(c.last, 1))
+       || LENGTH(c.first)
+       || LENGTH(c.last)
+       || UPPER(REPLACE(a.name, ' ', ''))
+       AS password
+FROM accounts a
+JOIN primary_poc p
+ON a.name = p.name
+JOIN company c
+ON a.name = c.name;
